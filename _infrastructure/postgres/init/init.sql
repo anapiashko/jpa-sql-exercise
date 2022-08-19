45a8f0866579be48db28e65721a7761c1e3b2450
Piashko, Anastasiya <Anastasiya.Piashko@wolterskluwer.com>
2022-08-19 18:37:52 +0300

    init commit

---
+++ _infrastructure/postgres/init/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE jpa_sql_exercise;
+CREATE USER postgre_user SUPERUSER;
+ALTER USER postgre_user WITH PASSWORD 'password'
---
+++ src/main/java/com/jpa/sql/exercise/JpaSqlExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.sql.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSqlExerciseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaSqlExerciseApplication.class, args);
+  }
+
+}
