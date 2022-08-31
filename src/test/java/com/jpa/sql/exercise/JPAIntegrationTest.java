bb44a55983a37ac462841af8a7565c3c54c22d3f
Piashko, Anastasiya <anapiashko@gmail.com>
2022-08-31 16:11:55 +0300

    added test DB h2

---
+++ src/test/java/com/jpa/sql/exercise/JPAIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.jpa.sql.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JpaSqlExerciseApplication.class)
+class JPAIntegrationTest {
+ 
+    @Autowired
+    private UserRepository UserRepository;
+
+    @Autowired private DataSource dataSource;
+    @Autowired private JdbcTemplate jdbcTemplate;
+    @Autowired private EntityManager entityManager;
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    void injectedComponentsAreNotNull(){
+        assertThat(dataSource).isNotNull();
+        assertThat(jdbcTemplate).isNotNull();
+        assertThat(entityManager).isNotNull();
+        assertThat(userRepository).isNotNull();
+    }
+
+    @Test
+    void whenSaveAndRetrieveEntityThenOK() {
+        User genericEntity = UserRepository.save(new User("1","test"));
+        Optional<User> foundEntity = UserRepository.findById(genericEntity.getId());
+
+        assertTrue(foundEntity.isPresent());
+        assertNotNull(foundEntity.get());
+    }
+}
